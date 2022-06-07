e6dea2f3fbf947d8a14c93a54e0303093fe6e539
kaustubhgupta1828@gmail.com <kaustubhgupta1828@gmail.com>
2022-06-07 09:38:32 +0000

    Created home; Edited settings and ServerModule1; Deleted Form1

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,8 +1,12 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import homeTemplate
 from anvil import *
+import plotly.graph_objects as go
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
-class Form1(Form1Template):
+class home(homeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
