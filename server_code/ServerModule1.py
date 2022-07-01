6a4103e49cf4440107198f5a1186dae4e76bae4e
kaustubhgupta1828@gmail.com <kaustubhgupta1828@gmail.com>
2022-07-01 13:02:42 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -16,91 +16,103 @@ API_KEY = anvil.secrets.get_secret('GITHUB_API_KEY')
 # Here is an example - you can replace it with your own:
 #
 @anvil.server.callable
-def fetchStats(username: str):
-  headers = {"Authorization": "bearer " + API_KEY}
-  query_top_language = {
-    "variables": {
-      "username": username
-    },
-    "query": """
-        query languageUsed($username: String!) {
-        user(login: $username) {
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
-            nodes {
-              languages(first: 50, orderBy: {field: SIZE, direction: DESC}) {
-                edges {
-                  node {
-                    name
+def fetchStats(username: str) -> tuple:
+    
+    headers = {"Authorization": "bearer " + API_KEY}
+    query_top_language = {
+        "variables": {
+          "username": username
+        },
+        "query": """
+            query languageUsed($username: String!) {
+            user(login: $username) {
+              repositories(ownerAffiliations: OWNER, isFork: false, last: 100) {
+                nodes {
+                  languages(first: 50, orderBy: {field: SIZE, direction: DESC}) {
+                    edges {
+                      node {
+                        name
+                      }
+                    }
                   }
                 }
               }
             }
-          }
-        }
-      },
-    """
-  }
-  
-  query_general_info = {
-    "variables": {
-      "username": username
-    },
-    "query": """
-      query userInfo($username: String!) {
-      user(login: $username) {
-        name
-        login
-        contributionsCollection {
-          totalCommitContributions
-          restrictedContributionsCount
-        }
-        repositoriesContributedTo(
-          first: 1
-          contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]
-        ) {
+          },
+        """
+    }
+    
+    query_general_info = {
+        "variables": {
+          "username": username
+        },
+        "query": """
+          query userInfo($username: String!) {
+          user(login: $username) {
+            name
+    login
+    contributionsCollection {
+      totalCommitContributions
+      restrictedContributionsCount
+    }
+    repositoriesContributedTo(
+      first: 1
+      contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]
+    ) {
+      totalCount
+    }
+    pullRequests(first: 1) {
+      totalCount
+    }
+    openIssues: issues(states: OPEN) {
+      totalCount
+    }
+    closedIssues: issues(states: CLOSED) {
+      totalCount
+    }
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    repositories(
+      first: 100
+      ownerAffiliations: OWNER
+      orderBy: {direction: DESC, field: STARGAZERS}
+    ) {
+      totalCount
+      nodes {
+        stargazers {
           totalCount
-        }
-        pullRequests(first: 1) {
-          totalCount
-        }
-        openIssues: issues(states: OPEN) {
-          totalCount
-        }
-        closedIssues: issues(states: CLOSED) {
-          totalCount
-        }
-        followers {
-          totalCount
-        }
-        repositories(
-          first: 100
-          ownerAffiliations: OWNER
-          orderBy: {direction: DESC, field: STARGAZERS}
-        ) {
-          totalCount
-          nodes {
-            stargazers {
-              totalCount
-            }
-          }
         }
       }
     }
-    """
+    avatarUrl
+    bio
+    location
+    twitterUsername
   }
-  r1 = s.post( 
-           "https://api.github.com/graphql",
-            json=query_top_language,
-            headers=headers,
-            timeout=15,
-        )
-  
-  r2 = s.post( 
-           "https://api.github.com/graphql",
-            json=query_general_info,
-            headers=headers,
-            timeout=15,
-        )
-  language_data = r1.json()
-  general_data = r2.json()
-  return (general_data, language_data)
+        }
+        """
+    }
+    
+    r1 = s.post( 
+               "https://api.github.com/graphql",
+                json=query_top_language,
+                headers=headers,
+                timeout=15,
+    )
+    
+    r2 = s.post( 
+               "https://api.github.com/graphql",
+                json=query_general_info,
+                headers=headers,
+                timeout=15,
+    
+    )
+    
+    language_data = r1.json()
+    general_data = r2.json()
+    
+    return (general_data, language_data)
