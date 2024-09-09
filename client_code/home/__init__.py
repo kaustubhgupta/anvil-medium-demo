b583a2be42794b25ea9dd9c5fa05b7de9390675d
kaustubhgupta1828@gmail.com <kaustubhgupta1828@gmail.com>
2024-09-09 17:15:35 +0000

    Edited home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,9 +1,6 @@
 from ._anvil_designer import homeTemplate
 from anvil import *
 import plotly.graph_objects as go
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
 
 class home(homeTemplate):
